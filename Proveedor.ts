dbee10ba9a2fe0453f5a34492e8d45168cfac833
Facundo Tucker <facutucker05@gmail.com>
2024-11-19 13:27:45 -0300

    nuevos setters en RedVeterinaria y pequeña correcion en los telefonos

--- Proveedor.ts
+++ Proveedor.ts
@@ -3,9 +3,9 @@ import { GeneradorID } from "./GeneradorID";
 export class Proveedor {
     private id: number;
     private nombre: string;
-    private telefono: string;
+    private telefono: number;
 
-    constructor(nombre: string, telefono: string, generador: GeneradorID) {
+    constructor(nombre: string, telefono: number, generador: GeneradorID) {
         this.id = generador.generarIDUnico(); // usamos el generador de ID único
         this.nombre = nombre;
         this.telefono = telefono;
@@ -15,8 +15,8 @@ export class Proveedor {
          return this.nombre;
     }
 
-    public getTelefono(): string {
-        return this.nombre;
+    public getTelefono(): number {
+        return this.telefono;
     }
 
     public getID(): number {
@@ -26,7 +26,7 @@ export class Proveedor {
     public setNombre(nombre: string): void {
         this.nombre = nombre;
     }
-    public setTelefono(telefono: string): void {
+    public setTelefono(telefono: number): void {
         this.telefono = telefono;
     }
 }
