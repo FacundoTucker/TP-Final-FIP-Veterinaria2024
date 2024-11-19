939df6c0b41808f30e2f3569e220b62122505174
Maico Olguin <168041229+cuentahitgub1@users.noreply.github.com>
2024-11-19 20:02:25 -0300

    metodoEliminar agregado a RedVeterinarias.ts

--- RedVeterinarias.ts
+++ RedVeterinarias.ts
@@ -6,39 +6,59 @@ import { Proveedor } from "./Proveedor";
 export class RedVeterinarias {
     private veterinarias: Veterinaria[] = [];
     private clientes: Cliente[] = [];
-    private pacientes: Paciente[] = [];
+    //private pacientes: Paciente[] = [];
     private proveedores: Proveedor[] = [];
 
+    //metodos agregar
     public agregarVeterinaria(veterinaria: Veterinaria): void {
         this.veterinarias.push(veterinaria);
     }
-
     public agregarCliente(cliente: Cliente): void {
         this.clientes.push(cliente);
     }
-
-    public agregarPaciente(paciente: Paciente): void {
-        this.pacientes.push(paciente);
-    }
-
     public agregarProveedor(proveedor: Proveedor): void {
         this.proveedores.push(proveedor);
+    }
+
+    //metodos eliminar
+    public eliminarVeterinaria(veterinariaAEliminar: Veterinaria): void {
+        const index = this.veterinarias.indexOf(veterinariaAEliminar);
+        if (index !== -1) this.veterinarias.splice(index, 1);
+    }
+    public eliminarCliente(clienteAEliminar: Cliente): void {
+        const index = this.clientes.indexOf(clienteAEliminar);
+        if (index !== -1) this.clientes.splice(index, 1);
+    }
+    public eliminarProveedor(proveedorAEliminar: Proveedor): void {
+        const index = this.proveedores.indexOf(proveedorAEliminar);
+        if (index !== -1) this.proveedores.splice(index, 1);
     }
 
     // Getters
     public getVeterinarias(): Veterinaria[] {
         return this.veterinarias;
     }
-
     public getClientes(): Cliente[] {
         return this.clientes;
     }
-
-    public getPacientes(): Paciente[] {
+    /*public getPacientes(): Paciente[] {
         return this.pacientes;
-    }
-
+    }*/
     public getProveedores(): Proveedor[] {
         return this.proveedores;
+    }
+
+    //setters
+    public setVeterinaria(veterinariaAModificar : Veterinaria, nombreNuevo : string, direccionNueva : string) : void {
+        veterinariaAModificar.setNombre(nombreNuevo);
+        veterinariaAModificar.setDireccion(direccionNueva);
+    }
+    public setCliente(clienteAModificar : Cliente, nombreNuevo : string, telefonoNuevo : number) : void {
+        clienteAModificar.setNombre(nombreNuevo);
+        clienteAModificar.setTelefono(telefonoNuevo);
+    }
+    public setProveedor(proveedorAModificar : Cliente, nombreNuevo : string, telefonoNuevo : number) : void {
+        proveedorAModificar.setNombre(nombreNuevo);
+        proveedorAModificar.setTelefono(telefonoNuevo);
     }
 }
