0c70c67f9b65dfcd20e707b8bbeba37435f25886
Maico Olguin <168041229+cuentahitgub1@users.noreply.github.com>
2024-11-20 20:33:44 -0300

    Add files via upload
    Cambios 20/11

--- main.ts
+++ main.ts
@@ -1,57 +1,26 @@
+import {menu} from "./menu";
 import { GeneradorID } from "./GeneradorID";
-import { Veterinaria } from "./Veterinaria";
+// import { Veterinaria } from "./Veterinaria";
 import { Cliente } from "./Cliente";
 import { Perro } from "./Perro";
 import { Gato } from "./Gato";
 import { Exotico } from "./Exotico";
-import { Proveedor } from "./Proveedor";
-import { RedVeterinarias } from "./RedVeterinarias";
+// import { Proveedor } from "./Proveedor";
+// import { RedVeterinarias } from "./RedVeterinarias";
 
 const generador = new GeneradorID();
 
-const redVeterinarias = new RedVeterinarias();
+// const redVeterinarias = new RedVeterinarias();
 
-const veterinaria = new Veterinaria("Veterinaria M1", "Vicente Lopez 2037", generador);
+// const veterinaria = new Veterinaria("Veterinaria M1", "Vicente Lopez 2037", generador);
 const clienteJuan = new Cliente("Juan Pérez", 123456789, generador);
-
 const pacienteFirulais = new Perro("Firulais", clienteJuan);
 const pacienteMichi = new Gato("Michi", clienteJuan);
 const pacienteLoro = new Exotico("Loro Pepe", clienteJuan, "pio pio");
 
-const proveedor = new Proveedor("Proveedor ABC", 123123123, generador);
+// const proveedor = new Proveedor("Proveedor ABC", 123123123, generador);
 
-redVeterinarias.agregarVeterinaria(veterinaria);
-redVeterinarias.agregarCliente(clienteJuan);
-redVeterinarias.agregarProveedor(proveedor);
+let Menu = new menu()
 
-clienteJuan.agregarMascota(pacienteFirulais);
-clienteJuan.agregarMascota(pacienteMichi);
-clienteJuan.agregarMascota(pacienteLoro);
+Menu.iniciar();
 
-clienteJuan.consultar(pacienteFirulais); //consulta con determinada mascota pero siempre el mismo dueño
-clienteJuan.consultar(pacienteFirulais);
-clienteJuan.consultar(pacienteFirulais);
-clienteJuan.consultar(pacienteMichi);
-clienteJuan.consultar(pacienteLoro);
-
-
-console.log("Veterinarias:", redVeterinarias.getVeterinarias());
-console.log("Clientes:", redVeterinarias.getClientes());
-console.log("Mascotas del cliente: ", clienteJuan.getMascotas());  //tambien podemos agregar las mascotas a la red como pacientes
-console.log("Proveedores:", redVeterinarias.getProveedores());
-
-redVeterinarias.setVeterinaria(veterinaria, "Veterinaria M3", "Coronel Suarez 5400");
-console.log(redVeterinarias.getVeterinarias());
-
-//lista eliminacion
-redVeterinarias.eliminarVeterinaria(veterinaria);
-redVeterinarias.eliminarProveedor(proveedor);
-redVeterinarias.eliminarCliente(clienteJuan);
-clienteJuan.eliminarMascota(pacienteFirulais);
-clienteJuan.eliminarMascota(pacienteMichi);
-clienteJuan.eliminarMascota(pacienteLoro);
-
-console.log("Veterinarias:", redVeterinarias.getVeterinarias());
-console.log("Clientes:", redVeterinarias.getClientes());
-console.log("Mascotas del cliente: ", clienteJuan.getMascotas());
-console.log("Proveedores:", redVeterinarias.getProveedores());
